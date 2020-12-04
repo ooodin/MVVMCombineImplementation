2f0b33eb9df9588b034c29c61032cc4db74d20f0
Artem Semavin <Artem Semavin>
2020-12-03 19:44:13 -0500

    Initial Commit

---
+++ MVPImplementation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVPImplementation
+//
+//  Created by Artem Semavin on 03.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
